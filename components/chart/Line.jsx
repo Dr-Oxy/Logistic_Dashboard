e7d9f85cfbde69e879ae891ef79798d2be0780e4
droxy <chiomaotu22@gmail.com>
2022-02-28 09:41:24 +0100

    resized chart

--- components/chart/Line.jsx
+++ components/chart/Line.jsx
@@ -35,19 +35,23 @@ const data = {
   datasets: [
     {
       label: 'Shipments',
-      data: [10000, 15000, 12000, 5800, 46000, 3600, 22000, 60000],
-      fill: false,
+      data: [10000, 15000, 30000, 36000, 46000, 36000, 28000, 60000],
+      fill: true,
       borderColor: 'rgba(44, 217, 197)',
       backgroundColor: 'rgba(44, 217, 197)',
-      tension: 0.1,
+      tension: 0.2,
+
+      borderWidth: 1,
     },
     {
       label: 'Vehicles',
-      data: [2500, 3000, 6000, 8200, 14000, 7200, 4000, 40000],
+      data: [200, 3000, 6000, 8200, 14000, 7200, 4000, 40000],
       fill: true,
       borderColor: 'rgba(102, 114, 251)',
       backgroundColor: 'rgba(102, 114, 251)',
-      tension: 0,
+      tension: 0.2,
+
+      borderWidth: 1,
     },
   ],
 };
@@ -58,12 +62,13 @@ const options = {
       ticks: { beginAtZero: true },
     },
   },
+  maintainAspectRatio: false,
 };
 
 const LineChart = () => {
   return (
-    <div className="p-5 md:px-10 py-5 ">
-      <Line data={data} options={options} />
+    <div className="p-5 md:px-10 py-5">
+      <Line data={data} options={options} height={300} width={500} />
     </div>
   );
 };
