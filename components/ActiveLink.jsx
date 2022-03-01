744a48834d7d0b135f632e78b838b219b6a9c82f
droxy <chiomaotu22@gmail.com>
2022-03-01 15:10:26 +0100

    Refactor: rewrote the active link color change

--- components/ActiveLink.jsx
+++ components/ActiveLink.jsx
@@ -11,21 +11,10 @@ const ActiveLink = ({ children, href }) => {
     router.push(href);
   };
 
-  //   const style = {
-  //     color: isCurrentPath ? '#fff' : 'rgba(1, 27, 87, 0.75)',
-  //     backgroundColor: isCurrentPath ? 'rgba(56, 108, 226)' : 'transparent',
-  //   };
-
-  const bg1 = 'bg-custom-blue';
-  const bg2 = 'bg-transparent';
-
-  const text1 = 'text-white';
-  const text2 = 'text-dark-blue';
-
   return (
     <a
       href={href}
-      className={isCurrentPath ? `${bg1} ${text1}` : `${bg2} ${text2}`}
+      className={isCurrentPath ? 'active' : ''}
       onClick={handleClick}
     >
       {children}
