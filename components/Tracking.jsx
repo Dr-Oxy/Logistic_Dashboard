63d8a8d9a113abcd10e186d56e236e6555405480
droxy <chiomaotu22@gmail.com>
2022-03-31 08:48:52 +0100

    color and border adjustments

--- components/Tracking.jsx
+++ components/Tracking.jsx
@@ -7,7 +7,7 @@ const Tracking = () => {
     <div className="flex items-center justify-center">
       <div className="h-72 mt-4  flex items-center">
         <div className=" mx-auto lg:mx-0 h-28 w-28 rounded-full flex items-center justify-center bg-blue-pulse animate-beat">
-          <GoSearch className="text-2xl text-dark-blue" />
+          <GoSearch className="text-3xl text-dark-blue dark:text-white/60" />
         </div>
       </div>
     </div>
